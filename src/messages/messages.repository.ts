6296c6495fec3eb81833599b770b6f62e91d2c43
Dave <dave.cadelina@outlook.com>
2023-05-18 01:40:24 +1000

    added service and repository

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,21 @@
+import { readFile, writeFile } from 'fs/promises'
+
+export class MessagesRepository {
+
+
+    async findOne(id: string) {
+        const contents = await readFile('messages.json', 'utf8')
+        const messages = JSON.parse(contents)
+
+        return messages[id]
+    }
+
+    async findAll() {
+
+    }
+
+    async create(message: string) {
+
+    }
+
+}
