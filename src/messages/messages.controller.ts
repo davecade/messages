82b3a41ebbb941bbd212ec21981c0db56d869b0d
Dave <dave.cadelina@outlook.com>
2023-05-28 18:01:12 +1000

    just before doing DI

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,42 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common'
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common'
 import { CreateMessageDto } from './dtos/create-message.dto'
+import { MessagesService } from './messages.service'
+
+interface Repository {
+    findOne(id: string)
+    findAll()
+    create(content: string)
+}
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessagesService
+
+    constructor() {
+        // The Controller is creating its own dependencies
+        // DONT DO THIS ON REAL APPS
+        this.messagesService = new MessagesService()
+    }
+
     @Get()
     listMessages() {
-
+        return this.messagesService.findAll()
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto) {
-        console.log("Body >> ", body)
+        const content = body.content
+        return this.messagesService.create(content)
     }
 
     @Get('/:id')
-    getMessage(@Param() id: string) {
-        console.log("ID >> ", id)
+    async getMessage(@Param('id') id: string) {
+
+        const message = await this.messagesService.findOne(id)
+
+        if(!message) {
+            throw new NotFoundException('message not found')
+        }
+        return message
     }
 }
